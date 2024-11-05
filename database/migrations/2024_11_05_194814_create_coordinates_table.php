e87c394006340e09313638dbfd9b3f3fe2fa9c34
Pablo Hernandez Villegas <38545507+Pblo16@users.noreply.github.com>
2024-11-05 16:28:54 -0600

    Create migration files for coordinates, personals, and process tables with necessary fields and relationships

---
+++ database/migrations/2024_11_05_194814_create_coordinates_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('coordinates', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('row');
+            $table->integer('column_start');
+            $table->integer('column_end');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('coordinates');
+    }
+};
